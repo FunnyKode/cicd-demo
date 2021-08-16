e2b5352391bb30b7228381560addf2dbfdf3c624
Shekinah Nageza <shekinahnzinga@gmail.com>
2021-08-16 12:09:26 +0100

    changes minor

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -6,7 +6,7 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const [pending, setPending] = useState(true);
-
+// je suis là
     useEffect(() => {
         app.auth().onAuthStateChanged(user => {
             setCurrentUser(user);
